a9c91701b59c52e1269bd5fb290404aef7f0c227
Thomas Koch <thomas.koch@ymc.ch>
2010-07-09 16:50:26 +0200

    bugfixes in I18N

--- I18n/src/date_format_symbols.php
+++ I18n/src/date_format_symbols.php
@@ -2,17 +2,25 @@
 
 class ymcI18nDateFormatSymbols
 {
-    private $locale;
+    private $localeArray;
+    private $localeString;
 
-    private $instances = array();
+    private static $instances = array();
 
     private $properties = array();
 
+    private static $dateFieldDelimiter = array( 
+    
+    );
+
     public function __construct( $locale )
     {
-        $this->locale = $locale;    
+        $this->localeArray = Locale::parseLocale( $locale );
+        $this->localeString = Locale::composeLocale( $this->localeArray );
+
         $this->initMonths();
         $this->initDays();
+        $this->initPattern();
     }
 
     private function initMonths()
@@ -22,7 +30,7 @@ class ymcI18nDateFormatSymbols
             'monthsLong'  => array()
         );
 
-        $formatter = new IntlDateFormatter( $this->locale, IntlDateFormatter::FULL,
+        $formatter = new IntlDateFormatter( $this->localeString, IntlDateFormatter::FULL,
                IntlDateFormatter::FULL, 'GMT', IntlDateFormatter::GREGORIAN, 'LLL@LLLL' );
 
         $date = new DateTime( '1970-01-05', new DateTimeZone( 'GMT' ) );
@@ -49,10 +57,11 @@ class ymcI18nDateFormatSymbols
         );
         $daysShort = array();
 
-        $formatter = new IntlDateFormatter( $this->locale, IntlDateFormatter::FULL,
+        $formatter = new IntlDateFormatter( $this->localeString, IntlDateFormatter::FULL,
                IntlDateFormatter::FULL, 'GMT', IntlDateFormatter::GREGORIAN, 'eeeee@eee@eeee' );
 
-        $date = new DateTime( '1970-01-05', new DateTimeZone( 'GMT' ) );
+        // start date must be sunday so that sunday will be the first element in the array
+        $date = new DateTime( '1970-01-04', new DateTimeZone( 'GMT' ) );
 
         for( $i = 0; $i < 7; ++$i )
         {
@@ -75,6 +84,63 @@ class ymcI18nDateFormatSymbols
         $this->properties = array_merge( $this->properties, $properties );
     }
 
+    private function initPattern()
+    {
+        $formatter = new IntlDateFormatter( $this->localeString, IntlDateFormatter::SHORT,
+               IntlDateFormatter::NONE);
+        $pattern = $formatter->getPattern();
+
+        $properties = array( 
+            'dateFieldDelimiter' => NULL,
+            'MDY_dayPosition' => NULL,
+            'MDY_monthPosition' => NULL,
+            'MDY_yearPosition' => NULL
+        );
+
+        $position = 0;
+        for( $i=0; $i<strlen( $pattern ); ++$i )
+        {
+            switch( substr( $pattern, $i, 1 ) )
+            {
+                case 'y':
+                case 'Y':
+                case 'u':
+                    if( NULL === $properties['MDY_yearPosition'] )
+                    {
+                        $properties['MDY_yearPosition'] = ++$position;
+                    }
+                break;
+
+                case 'M':
+                case 'L':
+                    if( NULL === $properties['MDY_monthPosition'] )
+                    {
+                        $properties['MDY_monthPosition'] = ++$position;
+                    }
+                break;
+
+                case 'd':
+                    if( NULL === $properties['MDY_dayPosition'] )
+                    {
+                        $properties['MDY_dayPosition'] = ++$position;
+                    }
+                break;
+
+                case ' ':
+                    // ignore
+                break;
+
+                default:
+                    if( NULL === $properties['dateFieldDelimiter'] )
+                    {
+                        $properties['dateFieldDelimiter'] = substr( $pattern, $i, 1 );
+                    }
+                break;
+            }
+        }
+        $this->properties = array_merge( $this->properties, $properties );
+    }
+
     public function __get( $name )
     {
         if( !array_key_exists( $name, $this->properties ) )
@@ -84,7 +150,7 @@ class ymcI18nDateFormatSymbols
         return $this->properties[$name];
     }
 
-    public function getInstance( $locale )
+    public static function getInstance( $locale )
     {
         if( !array_key_exists( $locale, self::$instances ) )
         {
